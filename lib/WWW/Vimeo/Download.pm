6e8cbc401a45dc5eeb979f1d809edc51aee085c6
Alessandro Ghedini <alexbio@cpan.org>
2010-06-29 14:21:16 +0000

    Import of ALEXBIO/WWW-Vimeo-Download-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-Vimeo-Download
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ALEXBIO/WWW-Vimeo-Download-0.02.tar.gz
    gitpan-cpan-author:       ALEXBIO
    gitpan-cpan-maturity:     released

--- lib/WWW/Vimeo/Download.pm
+++ lib/WWW/Vimeo/Download.pm
@@ -14,11 +14,11 @@ WWW::Vimeo::Download - Very simple Vimeo video downloader.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
