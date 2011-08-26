9d0677897a3ec1babaeda31a7b52b36ed15768de
Hernan Lopes <hernan@cpan.org>
2011-08-26 00:57:06 +0000

    Import of HERNAN/WWW-Vimeo-Download-0.01 from CPAN.
    gitpan-cpan-distribution: WWW-Vimeo-Download
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         HERNAN/WWW-Vimeo-Download-0.01.tar.gz
    gitpan-cpan-author:       HERNAN
    gitpan-cpan-maturity:     released

--- lib/WWW/Vimeo/Download.pm
+++ lib/WWW/Vimeo/Download.pm
@@ -1,180 +1,257 @@
 package WWW::Vimeo::Download;
-
-use Carp;
+use Moose;
+use XML::XPath;
 use LWP::UserAgent;
-use HTTP::Request::Common;
-use XML::Simple;
+use HTTP::Request;
+use Perl6::Form;
+use utf8;
 
-use warnings;
-use strict;
+our $VERSION = '0.01';
+my $VER = $VERSION;
+
+has video_id => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => '',
+);
+
+has download_url => (
+    is  => 'rw',
+    isa => 'Str',
+);
+
+has [
+    qw/caption width height duration thumbnail totalComments totalLikes totalPlays url_clean url uploader_url uploader_portrait uploader_display_name nodeId isHD privacy isPrivate isPassword isNobody embed_code filename nfo filename_nfo/
+  ] => (
+    is  => 'rw',
+    isa => 'Any',
+  );
+
+has browser => (
+    is      => 'ro',
+    isa     => 'LWP::UserAgent',
+    default => sub {
+        my $ua = LWP::UserAgent->new();
+        $ua->agent('Windows IE 6');
+        return $ua;
+    },
+);
+
+has res => (    #browser response
+    is  => 'rw',
+    isa => 'Any',
+);
+
+has xml => (
+    is => 'rw',
+    isa => 'Any',
+);
+
+has target_dir => (
+    is => 'rw',
+    isa => 'Str',
+    default => './',
+);
+
+sub load_video {
+    my ( $self, $video_id_or_url ) = @_;
+    if ( defined $video_id_or_url ) {
+        if ( $video_id_or_url =~ m{http://www\.vimeo\.com/([^/]+)}i ) {
+            $self->video_id($1);
+        }
+        elsif ( $video_id_or_url =~ m{http://vimeo.com/groups/([^/]+)/videos/([^/]+)}i ) {
+            $self->video_id($2);
+        }
+        else {
+            $self->video_id($video_id_or_url);
+        }
+        $self->set_download_url();
+    }
+    else {
+        warn "Example usage: \$self->load_video( 'VIMEO_VIDEO_ID' ) "
+          and return 0;
+    }
+}
+
+sub download {
+    my ( $self ) = @_; 
+    warn "Please set a video url first. ex: \$vimeo->load_video( 'http://www.vimeo.com/27855315' ) " and return if ! $self->download_url;
+    $self->res( $self->browser->get( $self->download_url ) );
+    if ( defined $self->res and $self->res->is_success ) {
+        $self->prepare_nfo;
+        $self->save_nfo;
+        $self->save_video( $self->res->content );
+    }
+}
+
+sub save_video {
+    my ( $self, $video_data ) = @_; 
+    my $filename = $self->filename( $self->target_dir.'/'.$self->filename. '.mp4' );
+    open FH, ">$filename";
+    print FH $video_data;
+    close FH;
+}
+
+sub save_nfo {
+    my ( $self ) = @_; 
+    $self->filename_nfo( $self->target_dir.'/'.$self->filename. '.nfo' );
+    my $filename = $self->filename_nfo;
+    open FH, ">$filename";
+    print FH $self->nfo;
+    close FH;
+}
+
+  sub prepare_nfo {
+      my ( $self ) = @_;
+
+      my $info = form
+  "===============================================================================",
+  "--[ WWW::Vimeo::Download ]-----------------------------------------------------",
+  "                                                                               ",
+  "  {||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||} ",
+        "[" . $self->caption . "]",
+  "                                                                               ",
+  "  .............Title: {<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<} ",
+        $self->caption,
+  "  .........Video Url: {<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<} ",
+        $self->url,
+  "  ............Author: {<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<} ",
+        $self->uploader_display_name,
+  "  ........Author Url: {<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<} ",
+        $self->uploader_url,
+  "                                                                               ",
+"                                [ REVIEW ]                                     ",
+"                                                                               ",
+"  {[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[} ",
+      $self->thumbnail,
+"                                                                               ",
+"---------------------------------------------------[ version $VER by HERNAN ]--",
+"==============================================================================="
+      ,;
+$self->nfo( $info );
+}
+
+sub title_to_filename {
+    my ( $self, $title ) = @_;
+    $title =~ s/\W/-/ig;
+    $title =~ s/--{1,}/-/ig;
+    $title =~ s/^-|-$//ig;
+    $title =~ tr/àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ/aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY/ ;
+    return $title;
+}
+
+sub set_filename {
+    my ( $self ) = @_; 
+    $self->filename( $self->title_to_filename( $self->caption) );
+}
+
+sub set_download_url {
+    my ($self) = @_;
+
+    #sets the download_url for specified video
+    warn "getting download_url for video id: "
+      . "http://www.vimeo.com/moogaloop/load/clip:"
+      . $self->video_id;
+    $self->res(
+        $self->browser->get(
+            "http://www.vimeo.com/moogaloop/load/clip:" . $self->video_id
+        )
+    );
+    if ( $self->res->is_success ) {
+        $self->xml(XML::XPath->new( xml => $self->res->content ));
+
+        $self->caption( $self->xml->findvalue( '/xml/video/caption')) and $self->set_filename;
+        $self->width($self->xml->findvalue( '/xml/video/width'));
+        $self->height($self->xml->findvalue( '/xml/video/height'));
+        $self->duration($self->xml->findvalue( '/xml/video/duration'));
+        $self->thumbnail($self->xml->findvalue( '/xml/video/thumbnail'));
+        $self->totalComments($self->xml->findvalue( '/xml/video/totalComments'));
+        $self->totalLikes($self->xml->findvalue( '/xml/video/totalLikes'));
+        $self->totalPlays($self->xml->findvalue( '/xml/video/totalPlays'));
+        $self->url_clean($self->xml->findvalue( '/xml/video/url_clean'));
+        $self->url($self->xml->findvalue( '/xml/video/url'));
+        $self->uploader_url($self->xml->findvalue( '/xml/video/uploader_url'));
+        $self->uploader_portrait($self->xml->findvalue( '/xml/video/uploader_portrait'));
+        $self->uploader_display_name($self->xml->findvalue( '/xml/video/uploader_display_name'));
+        $self->nodeId($self->xml->findvalue( '/xml/video/nodeId'));
+        $self->isHD($self->xml->findvalue( '/xml/video/isHD'));
+        $self->privacy($self->xml->findvalue( '/xml/video/privacy'));
+        $self->isPrivate($self->xml->findvalue( '/xml/video/isPrivate'));
+        $self->isPassword($self->xml->findvalue( '/xml/video/isPassword'));
+        $self->isNobody($self->xml->findvalue( '/xml/video/isNobody'));
+        $self->embed_code($self->xml->findvalue( '/xml/video/embed_code'));
+
+        my $request_signature = $self->xml->findvalue('/xml/request_signature');
+        my $request_signature_expires =
+          $self->xml->findvalue('/xml/request_signature_expires');
+        my $caption = $self->xml->findvalue('/xml/video/caption');
+        my $is_hd   = $self->xml->findvalue('/xml/video/isHD');
+        my $quality = 'sd';
+        $quality = 'hd' if ( $is_hd eq '1' );
+        my $download_url_with_req_id =
+            "http://www.vimeo.com/moogaloop/play/clip:"
+          . $self->video_id . "/"
+          . $request_signature . "/"
+          . $request_signature_expires . "/?q="
+          . $quality;
+
+        $self->res(
+            $self->browser->request(
+                HTTP::Request->new( HEAD => $download_url_with_req_id )
+            )
+        );
+        my $video_download_url = $self->res->request->uri;
+        if ( defined $video_download_url ) {
+            $self->download_url( $video_download_url->as_string );
+        }
+        $self->xml( undef );
+    }
+    else {
+        warn "Could not get success response for req. error: "
+          . $self->res->status_line;
+    }
+
+}
 
 =head1 NAME
 
-WWW::Vimeo::Download - Very simple Vimeo video downloader.
-
-=head1 VERSION
-
-Version 0.02
-
-=cut
-
-our $VERSION = '0.02';
+    WWW::Vimeo::Download - interface to download vimeo videos
 
 =head1 SYNOPSIS
 
-WWW::Vimeo::Download is a simple Vimeo video downloader.
-
     use WWW::Vimeo::Download;
+    my $vimeo = WWW::Vimeo::Download->new();
+    $vimeo->load_video( 'XYZ__ID_VIDEO' );        #REQ videoid or video url
+    $vimeo->target_dir( '/home/catalyst/tmp/' );  #OPTIONAL target dir
+    $vimeo->download();                           #start download
+    print $vimeo->download_url;                   #print the url for download
 
-    my $video = WWW::Vimeo::Download->new('123456');
+=head1 DESCRIPTION
 
-    my $video_url = $video->get_video_url;
-
-    $video->download('test.flv');
-
-=head1 METHODS
-
-=head2 new( $video_id )
-
-Create a L<WWW::Vimeo::Download> object
-
-=cut
-
-sub new {
-	my ($class, $video_id) = @_;
- 
-	my $self = bless({id => $video_id}, $class);
-
-	return $self;
-}
-
-=head2 get_video_url
-
-Return given video download URL
-
-=cut
-
-sub get_video_url {
-	my $self = shift;
-
-	my $xml_url  = "http://www.vimeo.com/moogaloop/load/clip:".$self -> {'id'};
-	my $xml_data = get_request($xml_url);
-
-	my $xml_handler = XMLin($xml_data);
-
-	my $error = $xml_handler -> {error} -> {message};
-
-	croak("ERROR: $error") unless !$error;
-
-	my $req_sign 	 = $xml_handler -> {request_signature};
-	my $req_sign_exp = $xml_handler -> {request_signature_expires};
-
-	my $video_url = "http://www.vimeo.com/moogaloop/play/clip:".$self -> {'id'}."/$req_sign/$req_sign_exp/?q=sd";
-
-	return $video_url;
-}
-
-=head2 download( $output_file )
-
-Download the given video
-
-=cut
-
-sub download {
-	my ($self, $output_file) = @_;
-
-	croak("ERROR: Set output file") if !$output_file;
-
-	my $video_url = $self -> get_video_url;
-
-	get_request($video_url, $output_file);	
-}
-
-=head1 SUBROUTINES
-
-=head2 get_request( $url )
-
-Make a GET request.
-
-=cut
-
-sub get_request {
-	my $url = shift;
-	my @args = @_;
-	
-	my $ua = LWP::UserAgent -> new;
-	$ua -> agent("");
-
-	my $response = $ua -> request(HTTP::Request->new( GET => $url ), @args) -> as_string;
-	
-
-	my $status = (split / /,(split /\n/, $response)[0])[1];
-
-	croak "ERROR: Server reported status $status" if $status != 200;
-
-	my @data = split('\n\n', $response);
-
-	return $data[1];
-}
 
 =head1 AUTHOR
 
-Alessandro Ghedini, C<< <alexbio at cpan.org> >>
+    Hernan Lopes
+    CPAN ID: HERNAN
+    hernan < @t > gmail
+    https://github.com/hernan604
 
-=head1 BUGS
+=head1 COPYRIGHT
 
-Please report any bugs or feature requests to C<bug-www-vimeo-download at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-Vimeo-Download>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+    This program is free software; you can redistribute
+    it and/or modify it under the same terms as Perl itself.
 
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc WWW::Vimeo::Download
+    The full text of the license can be found in the
+    LICENSE file included with this module.
 
 
-You can also look for information at:
+=head1 SEE ALSO
 
-=over 4
-
-=item * Homepage
-
-L<http://alexlog.co.cc/projects/www-vimeo-download>
-
-=item * Git Repository
-
-L<http://github.com/AlexBio/WWW-Vimeo-Download>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-Vimeo-Download>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/WWW-Vimeo-Download>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/WWW-Vimeo-Download>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/WWW-Vimeo-Download/>
-
-=back
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2010 Alessandro Ghedini.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
+perl(1).
 
 =cut
 
-1; # End of WWW::Vimeo::Download
+1;
+
+# The preceding line will help the module return a true value
+
--- t/01-use.t
+++
@@ -1,8 +0,0 @@
-use Test::More tests => 1;
-
-use strict;
-use WWW::Vimeo::Download;
-
-my $video = WWW::Vimeo::Download->new;
-
-can_ok($video, qw(get_video_url download));
